dd6be1ac034f523a3ea96154c386ea1ab4e8a81c
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-03-31 02:21:10 -0300

    style: Menu layout finished

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,5 +1,17 @@
+import { CircularProgressbar } from "react-circular-progressbar"
+
 import * as S from "./styles"
+import "react-circular-progressbar/dist/styles.css"
 
 export default function Menu() {
-	return <S.Menu></S.Menu>
+	return (
+		<S.Menu>
+			<S.Option>Hábitos</S.Option>
+			<S.Progress>
+				<S.Option>Hoje</S.Option>
+				<CircularProgressbar value={66} />
+			</S.Progress>
+			<S.Option>Histórico</S.Option>
+		</S.Menu>
+	)
 }
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -4,10 +4,45 @@ export const Menu = styled.footer`
 	width: 100%;
 	height: 70px;
 	display: flex;
-	justify-content: space-between;
+	justify-content: space-around;
 	align-items: center;
 	position: fixed;
 	bottom: 0;
 	left: 0;
 	background-color: #fff;
+`
+
+export const Option = styled.p`
+	font-family: "Lexend Deca";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 17.976px;
+	line-height: 22px;
+	color: #52b6ff;
+`
+export const Progress = styled.div`
+	width: 91px;
+	height: 91px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-bottom: 40px;
+	border-radius: 50%;
+	background: #52b6ff;
+	padding: 6px;
+	${Option} {
+		position: absolute;
+		color: #fff;
+	}
+	.CircularProgressbar-path {
+		stroke: #fff;
+	}
+	.CircularProgressbar-trail {
+		stroke: #52b6ff;
+	}
+`
+
+export const ProgressBar = styled.div`
+	width: 4px;
+	height: 88px;
 `
