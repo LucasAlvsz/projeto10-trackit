8f450bfca360c8e3ca75bc6c83d45e181c4ff9f2
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-04-04 05:58:18 -0300

    fix: Default values in login input

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -15,8 +15,8 @@ export default function SingIn() {
 	const navigate = useNavigate()
 	const [isLoading, setIsLoading] = useState(false)
 	const [userData, setUserData] = useState({
-		email: "Akali@gmail.com",
-		password: "Akali",
+		email: "",
+		password: "",
 	})
 	if (localStorage.getItem("@trackit/userData")) {
 		loggedDataUpdate.setLoggedData(
